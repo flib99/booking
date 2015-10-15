d9a57da432f44f01e58b8ceeea5eaf5dcaacec4d
Josh Walls <me@joshwalls.co.uk>
2015-10-15 12:30:42 +0100

    started work on template

--- include/template.php
+++ include/template.php
@@ -1,1 +1,43 @@
-<?php
+
+
+<html>
+
+<head>
+
+<!-- bootstrap stuff -->
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+<title>PLACEHOLDER_title</title>
+</head>
+
+<body>
+<!-- top bar-->
+<nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+  <div class="container">
+  	<div class="navbar-header">
+      <a class="navbar-brand" href="#">PLACEHOLDER_name</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+  </div>
+</nav>
+<!--end top bar -->
+
+<!-- bottom bar -->
+<nav class="navbar navbar-default navbar-fixed-bottom">
+  <div class="container">
+	<ul class="nav navbar-nav">
+        <li><p>© Copyright 2015 Josh Walls. All Rights Reserved.</p></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Privacy</a></li> 
+        <li><a href="#">API</a></li> 
+      </ul>
+  </div>
+</nav>
+</body>
+
+</html>
