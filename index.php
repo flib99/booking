61c5b7f9185acd8a32e0210a7d09f6dbd0bc884f
Josh Walls <me@joshwalls.co.uk>
2015-10-22 12:19:38 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('include/template.php');
+
+?>
+
+<!--[if IE]>
+According to the conditional comment this is IE<br />
+<![endif]-->
+
+<div class="container">
+	<div class="jumbotron">
+		<h1>Welcome to PLACEHOLDER</h1> 
+		<p>PLACEHOLDER TEXT</p>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<h2>About</h2>
+			<p>PLACEHOLDER</p>
+			<p><a class="btn btn-default" href="about.php" role="button">More details &raquo;</a></p>
+		</div>
+		<div class="col-md-4">
+			<h2>Login</h2>
+			<p>Already have an account?<br>Click the button below to log in.</p>
+			<p><a class="btn btn-default" href="#" role="button">Login&nbsp<span class="glyphicon glyphicon-log-in"></span> </a></p>
+		</div>
+		<div class="col-md-4">
+			<h2>Register</h2>
+			<p>Don't have an account? Wan't an account?<br>Click below to create one.</p>
+			<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+		</div>
+	</div>
+</div>
